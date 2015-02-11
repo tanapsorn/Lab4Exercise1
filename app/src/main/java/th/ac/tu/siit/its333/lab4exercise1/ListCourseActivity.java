35f6f7200165737e2d00b00126ebd22ed6666ce4
tanapsorn <don-t.smile@hotmail.com>
2015-02-11 11:24:11 +0700

    lab4
    44444444444444

--- app/src/main/java/th/ac/tu/siit/its333/lab4exercise1/ListCourseActivity.java
+++ app/src/main/java/th/ac/tu/siit/its333/lab4exercise1/ListCourseActivity.java
@@ -4,13 +4,19 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 
-public class ListCourseActivity extends ActionBarActivity {
+public class ListCourseActivity extends ActionBarActivity
+        implements AdapterView.OnItemClickListener,
+        AdapterView.OnItemLongClickListener {
 
     CourseDBHelper helper;
     SimpleCursorAdapter adapter;
@@ -20,7 +26,25 @@ public class ListCourseActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_course);
 
+
+
+        helper = new CourseDBHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT _id, code, grade||' ('||credit||' credit)' g FROM course",null);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2, // A textview
+                cursor, // cursor to a data collection
+                new String[] {"code","g"}, // column to be displayed
+                new int[] {android.R.id.text1,android.R.id.text2}, // ID of textview to display
+                0);
+
+        ListView lv = (ListView)findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemLongClickListener(this);
     }
+
 
 
     @Override
@@ -44,4 +68,47 @@ public class ListCourseActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view,
+                            int position, long id) {
+        Log.d("course", id + " is clicked");
+    }
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                   int position, long id) {
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        int n = db.delete("course",
+                "_id = ?",
+                new String[]{Long.toString(id)});
+
+        if (n == 1) {
+            Toast t = Toast.makeText(this.getApplicationContext(),
+                    "Successfully deleted the selected item.",
+                    Toast.LENGTH_SHORT);
+            t.show();
+
+            // retrieve a new collection of records
+            Cursor cursor = db.rawQuery("SELECT _id, code, grade||' ('||credit||' credit)' g FROM course",null);
+
+            // update the adapter
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_2, // A textview
+                    cursor, // cursor to a data collection
+                    new String[] {"code","g"}, // column to be displayed
+                    new int[] {android.R.id.text1,android.R.id.text2}, // ID of textview to display
+                    0);
+
+            adapter.changeCursor(cursor);
+            ListView lv = (ListView)findViewById(R.id.listView);
+            lv.setAdapter(adapter);
+
+        }
+        db.close();
+        return true;
+    }
+
+
 }
+
